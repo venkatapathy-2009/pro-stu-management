6953618f246049d26c7f9d7c4cb89d708f120a11
venkatapathy <venkatapathy859@gmail.com>
2026-07-17 18:12:11 +0530

    second commit

--- project/settings.py
+++ project/settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+    
+    
 }
 
 
